af36fbfae0760a3efd4f65094ebb5a51b4466c05
wonjangcloud9 <venuscwj@naver.com>
2023-02-05 16:24:23 +0900

    counter

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -3,21 +3,20 @@ package com.example.practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.practice.ui.theme.PracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,12 +24,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                var count by remember {
+                    mutableStateOf(0)
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Text(
+                        text = count.toString(),
+                        fontSize = 30.sp,
+                    )
+                    Button(onClick = { count++ }) {
+                        Text(text = "Click me")
+                    }
                 }
             }
         }
@@ -40,8 +50,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(10) {
-            i -> Icon(
+        items(10) { i ->
+            Icon(
                 imageVector = Icons.Filled.Add,
                 contentDescription = "Add",
             )
